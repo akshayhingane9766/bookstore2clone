a490bb59cb302ebb528d652f5b33bb30ace66efa
akshayhingane9766 <akshayhingane812@gmail.com>
2022-11-16 18:18:55 +0530

    added mvc

---
+++ bookstore2clone/bookstore2clone/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bookstore2clone.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string index()
+        {
+            return "mvc home controller";
+        }
+    }
+}
